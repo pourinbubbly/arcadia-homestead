2ab113ca8b002b3248d4466c8011f9a7f07156bb
pourinbubbly <erols.0701@gmail.com>
2026-08-03 12:44:22 +0300

    refactor: Clean repository, remove legacy asset dumps and keep pure Arcadia Homestead project

--- check_coop.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-img = Image.open(r"C:\Users\user\Downloads\Sprout Lands - Sprites - Basic pack\Sprout Lands - Sprites - Basic pack\Objects\Free_Chicken_House.png")
-print("Free_Chicken_House.png size:", img.size)
-
-img.save('chicken_house_building.png')
-print("chicken_house_building.png saved!")
